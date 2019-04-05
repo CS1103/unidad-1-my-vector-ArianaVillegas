e54ee3251373de6cda3f9e6cf0c31fef1af9694f
Ariana Villegas <ariana.villegas@utec.edu.pe>
2019-04-05 17:58:57 -0500

    Cambie la estructura de los vectores dentro del namespace de UTEC

--- Vector.cpp
+++ Vector.cpp
@@ -1,21 +1,48 @@
 //
 // Created by rudri on 3/29/2019.
 //
-
+#include <iostream>
 #include "Vector.h"
 
 namespace UTEC {
     void vector::push_back(const int &value) {
         // Completarlo
+        int *arrtemp=new int[_size+1];
+        for(int i=0; i<_size; i++){
+            arrtemp[i]=_arr[i];
+        }
+        arrtemp[_size]=value;
+        _arr=arrtemp;
+        _size++;
     }
 
     void vector::pop_back() {
         // Completarlo
+        int *arrtemp=new int[_size-1];
+        for(int i=0; i<_size-1; i++){
+            arrtemp[i]=_arr[i];
+        }
+        _arr=arrtemp;
+        _size--;
+    }
+
+    void vector::insert(int pos, const int &value){
+        // Completarlo
+        int *arrtemp=new int[_size+1];
+        for(int i=0; i<pos; i++){
+            arrtemp[i]=_arr[i];
+        }
+        arrtemp[pos]=value;
+        for(int i=pos+1; i<_size; i++){
+            arrtemp[i]=_arr[i];
+        }
+        _arr=arrtemp;
+        _size++;
     }
 
     vector::vector() : _arr{nullptr}, _size(0) {}
     vector::~vector() {
-        // Completarlo
+        delete[] _arr;
     }
 
     int vector::size() {return _size;}
